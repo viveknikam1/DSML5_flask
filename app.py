0d4eb6b9f6ea82518fdcba365101af856b5b1119
viveknikam1 <47035774+viveknikam1@users.noreply.github.com>
2021-03-14 12:33:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import pickle
-app = Flask(__name__)
+app1 = Flask(__name__)
 
-@app.route('/')
+@app1.route('/')
 def fun1():
     return render_template('predict.html')
 
-@app.route('/predict', methods = ['POST'])
+@app1.route('/predict', methods = ['POST'])
 def fun2():
     name=request.form['ename']
     exp = float(request.form['yrsexp'])
@@ -14,9 +14,9 @@ def fun2():
     sal = model.predict([[exp]])
     return '<h1>Thanks for visiting us. Your Expected Salary is {} </h1>'.format(int(sal[0]))
 
-@app.route('/contactus')
+@app1.route('/contactus')
 def fun3():
     return 'Thanks for visiting contact us'
 
 if __name__=='__main__':
-    app.run(debug = True)
+    app1.run(debug = True)
